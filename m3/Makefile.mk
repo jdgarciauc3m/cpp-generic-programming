0aa9e28a4ae02c954da095f5e83279a0389c0227
J. Daniel Garcia <jdgarcia@inf.uc3m.es>
2024-04-14 19:53:30 +0200

    m3/l4: Fundamental concepts in the std library

--- m3/Makefile.mk
+++ m3/Makefile.mk
@@ -1,6 +1,7 @@
 include $(M3_L1_DIR)/Makefile.mk
 include $(M3_L2_DIR)/Makefile.mk
 include $(M3_L3_DIR)/Makefile.mk
+include $(M3_L4_DIR)/Makefile.mk
 
 $(FINAL_M3_PDF): $(M3_PARTS) $(CONFIG_SLIDES)
 	$(LATEXMK) -jobname=$(subst /,-,$(<D)) $(M3_DIR)/slides
