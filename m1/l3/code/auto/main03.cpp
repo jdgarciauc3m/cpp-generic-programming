41368ca4238ec1feeb8ac695dc53e274c37cb202
J. Daniel Garcia <jdgarcia@inf.uc3m.es>
2024-05-10 13:27:39 +0200

    m1: Examples for auto&& in loops

--- m1/l3/code/auto/main03.cpp
+++ m1/l3/code/auto/main03.cpp
@@ -1,25 +1,75 @@
+#include <format>
 #include <iostream>
+#include <vector>
 
-int f();
-
-void test1() {
-  //X x1 = f();
-  auto x1 = f(); // X x1
-
-  //X & x2 = f(); // Error. Cannot bind reference to temporal.
-  //auto & x2 = f(); // Error. Cannot bind reference to temporal.
-
-  auto && x3 = f();
+int f() {
+  return 0;
 }
 
-int & g();
+void test1() {
+  // X x1 = f();
+  auto x1 = f();  // X x1
+  std::cout << std::format("x1 = {}\n", x1);
+
+  // X & x2 = f(); // Error. Cannot bind reference to temporal.
+  // auto & x2 = f(); // Error. Cannot bind reference to temporal.
+
+  auto && x3 = f();
+  std::cout << std::format("x3 = {}\n", x3);
+}
+
+int max = 0;  // NOLINT
+
+int & g() {
+  return max;
+}
 
 void test2() {
-  //X x3 = g();
-  auto x3 = g(); // X x3
+  // X x1 = g();
+  auto x1 = g();  // X x1
+  std::cout << std::format("x1 = {}\n", x1);
 
-  //X & x4 = g();
-  auto & x4 = g(); // X & x4
+  // X & x2 = g();
+  auto & x2 = g();  // X & x2
+  std::cout << std::format("x2 = {}\n", x2);
 
-  auto && x5 = g();
+  auto && x3 = g();
+  std::cout << std::format("x3 = {}\n", x3);
+}
+
+auto make_vector() {
+  return std::vector{1, 2, 3};
+}
+
+void test3() {
+  std::vector<int> v1{1, 2, 3};
+  for (auto x : v1) { std::cout << std::format("{}\n", x); }
+  for (auto & x : v1) { std::cout << std::format("{}\n", x); }
+  for (auto && x : v1) { std::cout << std::format("{}\n", x); }
+}
+
+void test4() {
+  for (auto x : make_vector()) { std::cout << std::format("{}\n", x); }
+  for (auto & x : make_vector()) { std::cout << std::format("{}\n", x); }
+  for (auto && x : make_vector()) { std::cout << std::format("{}\n", x); }
+}
+
+template <typename R>
+void print(R && rng) {  // NOLINT
+  for (auto x : rng) { std::cout << std::format("{}\n", x); }
+  for (auto & x : rng) { std::cout << std::format("{}\n", x); }
+  for (auto && x : rng) { std::cout << std::format("{}\n", x); }
+}
+
+void test5() {
+  std::vector<int> v1{1, 2, 3};
+  print(v1);
+}
+
+int main() {
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
 }
