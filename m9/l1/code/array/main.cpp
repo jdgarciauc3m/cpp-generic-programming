d735a25af87bfdc2ca5e2c04455b2edd5cc4edae
J. Daniel Garcia <jdgarcia@inf.uc3m.es>
2024-05-15 14:07:59 +0200

    m9/l1: Arrays

--- m9/l1/code/array/main.cpp
+++ m9/l1/code/array/main.cpp
@@ -1,22 +1,50 @@
-#include <iostream>
 #include <array>
+#include <iostream>
+
+// NOLINTBEGIN(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
+template <typename T>
+void print(T const vec[], int size) {
+  for (int i = 0; i < size; ++i) {
+    std::cout << vec[i] << ' ';  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+  }
+  std::cout << '\n';
+}
+
+// NOLINTEND(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
+
+template <typename T, std::size_t N>
+void print(std::array<T, N> const & vec) {
+  for (auto x : vec) { std::cout << x << ' '; }
+  std::cout << '\n';
+}
+
+template <typename T, std::size_t N, std::size_t... I>
+void print_helper(std::array<T, N> const & vec,
+                  [[maybe_unused]] std::index_sequence<I...> seq) {
+  ((std::cout << (I==0?"":" , ") << std::get<I>(vec)),...);
+}
+
+template <typename T, std::size_t N>
+void print2(std::array<T, N> const & vec) {
+  std::cout << __PRETTY_FUNCTION__ << '\n';
+  print_helper(vec, std::make_index_sequence<N>{});
+}
 
 // NOLINTBEGIN(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
 // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
 void f1() {
   std::cout << __PRETTY_FUNCTION__ << '\n';
-  int vec1[5] {1, 2, 3, 4, 5};
-  for (auto x : vec1) {
-    std::cout << x << ' ';
-  }
-  std::cout << '\n';
+  int vec1[5]{1, 2, 3, 4, 5};
+  print(vec1, 5);  // NOLINT(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
 
-  double vec2[] {1, 2, 3, 4, 5};
-  for (auto x : vec2) {
-    std::cout << x << ' ';
-  }
-  std::cout << '\n';
+  int vec2[]{1, 2, 3, 4, 5};
+  print(vec2, 5);  // NOLINT(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+
+  // int vec3[5] {vec1};
+  // int vec4[5] = vec1;
+  // vec3 = vec1;
 }
+
 // NOLINTEND(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
 // NOLINTEND(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
 
@@ -24,27 +52,42 @@ void f1() {
 void f2() {
   std::cout << __PRETTY_FUNCTION__ << '\n';
   std::array<int, 5> vec1{1, 2, 3, 4, 5};
-  for (auto x : vec1) {
-    std::cout << x << ' ';
-  }
-  std::cout << '\n';
+  print(vec1);
 
   std::array vec2 = std::to_array<double>({1, 2, 3, 4, 5});
-  for (auto x : vec2) {
-    std::cout << x << ' ';
-  }
-  std::cout << '\n';
+  print(vec2);
 
   std::array vec3{1, 2, 3, 4, 5};
-  for (auto x : vec3) {
-    std::cout << x << ' ';
-  }
-  std::cout << '\n';
+  print(vec3);
 
+  std::array vec4{vec1};
+  print(vec4);
+  std::get<2>(vec3) = 99;
+  vec4              = vec3;
+  print2(vec4);
 }
+
+
+
+void f3() {
+  std::array vec{1, 2, 3, 4, 5};
+  auto & vec2 = vec;
+  constexpr auto sz = std::tuple_size_v<std::decay_t<decltype(vec2)>>;
+  using basetype = std::tuple_element_t<0,decltype(vec)>;
+  std::array<std::string, sz> names;
+  for(std::size_t i=0; auto x : vec) {
+    names[i++] = std::to_string(x); // NOLINT
+  }
+  for (auto const &s : names) {
+    std::cout << s << '\n';
+  }
+}
+
 // NOLINTEND(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+
 
 int main() {
   f1();
   f2();
+  f3();
 }
